61e48e9850245eef3e8891b1341e8293e63a1fef
paziolka <ola.pazio@gmail.com>
2024-03-19 11:45:04 +0100

    Test WIP

--- test/test_main.py
+++ test/test_main.py
@@ -1,11 +1,39 @@
-# test_app.py
-
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app, get_db
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# import pytest
 
 client = TestClient(app)
+
+# @pytest.fixture
+# def override_get_db():
+#     engine = create_engine('postgresql://geo_projects_test:geo_projects_test@localhost:5432/geo_projects_test')
+#     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+#     try:
+#         yield TestingSessionLocal()
+#     finally:
+#         TestingSessionLocal().close()
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"hello": "world"}
+
+# def test_list_projects():
+#     response = client.get("/projects")
+#     assert response.status_code == 200
+#     assert response.json() == []
+
+# def test_create_project():
+#     project_data = {"name": "Test Project"}
+#     response = client.post("/projects", json=project_data)
+#     assert response.status_code == 200
+
+# def test_create_project_invalid_data():
+#     project_data = {"invalid_key": "Invalid Value"}
+#     response = client.post("/projects", json=project_data)
+#     assert response.status_code == 422
+
